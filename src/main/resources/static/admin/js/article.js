e4b41548d6043ba7fcab2d44a8999bfd4e56b1d2
jesse-liu <2683961691@qq.com>
2020-05-06 18:01:22 +0800

    调整一些页面显示问题

--- src/main/resources/static/admin/js/article.js
+++ src/main/resources/static/admin/js/article.js
@@ -40,7 +40,7 @@ function subArticle(status) {
     }
     $('#content-editor').val(content);
     $("#articleForm #status").val(status);
-    $("#articleForm #categories").val($('#multiple-sel').val());
+    //$("#articleForm #categories").val($('#multiple-sel').val());
     var params = $("#articleForm").serialize();
     var url = $('#articleForm #cid').val() != '' ? '/admin/article/modify' : '/admin/article/publish';
     tale.post({
